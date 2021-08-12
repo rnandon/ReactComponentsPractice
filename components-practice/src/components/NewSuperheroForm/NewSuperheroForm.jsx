2bc1b8a60e07c75b3d52756466ef9ffdc97b0d2b
Ryan <ryan@ryannd.com>
2021-08-12 11:30:33 -0500

    Revised NewSuperheroForm component appearance

--- components-practice/src/components/NewSuperheroForm/NewSuperheroForm.jsx
+++ components-practice/src/components/NewSuperheroForm/NewSuperheroForm.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Form from 'react-bootstrap/Form'
 
 
 class NewSuperheroForm extends Component{
@@ -32,22 +33,40 @@ class NewSuperheroForm extends Component{
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <h1>Create a New Superhero!</h1>
-                <label>
-                    Name:
-                    <input name="name" value={this.state.name} onChange={this.handleChange} />
-                </label>
-                <label>
-                    Primary Superpower:
-                    <input name="primaryAbility" value={this.state.primaryAbility} onChange={this.handleChange} />
-                </label>
-                <label>
-                    Secondary Superpower:
-                    <input name="secondaryAbility" value={this.state.secondaryAbility} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+            <div className="col-md-6 p-5">
+                <Form>
+                    <Form.Group className="mb-3" controlId="name">
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control type="text" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="primaryAbility">
+                        <Form.Label>Primary Superpower:</Form.Label>
+                        <Form.Control type="text" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="secondaryAbility">
+                        <Form.Label>Secondary Superpower:</Form.Label>
+                        <Form.Control type="text" />
+                    </Form.Group>
+                </Form>
+            </div>
+            // <form onSubmit={this.handleSubmit}>
+            //     <h1>Create a New Superhero!</h1>
+            //     <label>
+            //         Name:
+            //         <input name="name" value={this.state.name} onChange={this.handleChange} />
+            //     </label>
+            //     <br />
+            //     <label>
+            //         Primary Superpower:
+            //         <input name="primaryAbility" value={this.state.primaryAbility} onChange={this.handleChange} />
+            //     </label>
+            //     <br />
+            //     <label>
+            //         Secondary Superpower:
+            //         <input name="secondaryAbility" value={this.state.secondaryAbility} onChange={this.handleChange} />
+            //     </label>
+            //     <input type="submit" value="Submit" />
+            // </form>
         )
     }
 }
